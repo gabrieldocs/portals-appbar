4db7ef82969cb8b62e5cb5f938839a71a1323015
Lucas Gabriel Guilherme dos Santos <lucasggs.com@gmail.com>
2022-11-24 22:49:46 -0300

    added appbar

---
+++ index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PortalsAppBar() {
+    return(
+        <React.Fragment>
+            Current datetime: {new Date().getTime()}
+        </React.Fragment>
+    )
+}
